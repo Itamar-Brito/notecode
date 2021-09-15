b60c405fef266f5175bccb720d7f356196b0387c
Itamar-Brito <itamar.brito@gmail.com>
2021-09-15 00:12:05 -0300

    testando

--- src/resources/views/auth/login.blade.php
+++ src/resources/views/auth/login.blade.php
@@ -24,7 +24,7 @@
                 <x-jet-label for="password" value="{{ __('geral.password') }}" />
                 <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
             </div>
-
+            
             <div class="block mt-4">
                 <label for="remember_me" class="flex items-center">
                     <x-jet-checkbox id="remember_me" name="remember" />
