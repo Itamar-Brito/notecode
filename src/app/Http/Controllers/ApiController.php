320ed129ca91ebbb3065fcf7966721e3c42e8fbd
Itamar-Brito <itamar.brito@gmail.com>
2021-09-15 21:51:06 -0300

    receiving cep pro VIA CEP

--- src/app/Http/Controllers/ApiController.php
+++ src/app/Http/Controllers/ApiController.php
@@ -77,4 +77,15 @@ class ApiController extends Controller
 
         }
 
+
+        public function adressofUser($id) {
+
+            $cep = User::where('id', $id)->first();
+
+             // https://viacep.com.br/ws/83070152/json/
+            $jsonurl = "https://viacep.com.br/ws/" . $cep->cep . "/json/";
+            $json = file_get_contents($jsonurl);
+                return response($json, 200);
+        }
+
 }
