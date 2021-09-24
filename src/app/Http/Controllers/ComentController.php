501a80159ea01e387431c55984fa606c4e1931c2
Itamar-Brito <itamar.brito@gmail.com>
2021-09-24 00:08:04 -0300

    comentController

---
+++ src/app/Http/Controllers/ComentController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Note;
+use App\Models\User;
+use App\Repository\NotesRepository;
+
+class ComentController extends Controller
+{
+    
+
+}
