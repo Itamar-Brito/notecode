5b0b1f8d4b9ecb536a665d13baae9830d9d6070f
Itamar-Brito <itamar.brito@gmail.com>
2022-06-08 01:03:37 -0300

    assert true pipeline

---
+++ src/tests/Feature/UpdatePasswordTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Tests\Feature;
+
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+use Tests\TestCase;
+
+class UpdatePasswordTest extends TestCase
+{
+    public function test_password_can_be_updated()
+    {
+         $this->assertTrue(true);
+    }
+}
